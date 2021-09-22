e938ce5134187e8e1bb7a8d9c9313f69cba72108
ditsky <samruditsky@yahoo.com>
2021-09-21 20:42:58 -0400

    Add Player base class for Scout to inherit + comments and housekeeping

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,182 @@
+using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Networking;
+using MLAPI;
+
+public class Player : MonoBehaviour
+{
+    // Camera attached to this player
+    public Camera playerCamera; 
+    // Floating point variable to store the player's movement speed
+    public float speed; 
+    // Int to store player's throw power               
+    public int gains;
+    // Bool which determines if this is a local player or a client
+    public bool isLocalPlayer = true;
+    // Physics body for the game disc
+    public Rigidbody2D disc;
+    // Counter for remaining ability uses
+    public int abilityCount;
+    // SoundEffects object used to play all SFX
+    public SoundEffects soundEffects;
+    // Stores a reference to the Rigidbody2D component required to use 2D Physics
+    public Rigidbody2D playerBody;        
+    // Stores a reference to the arena being played in
+    private Arena arena;
+    // Stores a reference to the player sprite
+    private SpriteRenderer sprite;
+    // Bool which determines whether or not we are playing in training mode
+    public bool trainingMode = false;
+    // Bool which determines whether or not this player has possesion of the ball
+    private bool possession = false;
+
+    // Use this for initialization
+    public void Start()
+    {
+        // If this is the local player
+        if (isLocalPlayer)
+        {
+            // Enable this player's camera
+            playerCamera.enabled = true;
+        }
+        else
+        {
+            // Disable other player cameras
+            playerCamera.enabled = false;
+        }
+        
+        // Retrieve game object references from the component/scene
+        arena = FindObjectOfType<Arena>();
+        playerBody = GetComponent<Rigidbody2D>();
+        sprite = GetComponent<SpriteRenderer>();
+        soundEffects = GetComponent<SoundEffects>();
+        disc = GameObject.Find("Disc").GetComponent<Rigidbody2D>();
+        
+        // Keep physics body from spinning
+        playerBody.freezeRotation = true;
+
+        // Possibly change to a label or other identification later
+        trainingMode = arena.name == "TrainingArena";
+
+        // If Training is Enabled, enable the training mods
+        if (true == trainingMode)
+        {
+            TrainingMods training = gameObject.AddComponent<TrainingMods>() as TrainingMods;
+            // Give the Training Mods a reference to the disc
+            training.Load(disc);
+        }
+    }
+
+    public void Update()
+    {
+        // If player left clicks, throw the disc
+        if (possession && Input.GetButtonDown("Fire1"))
+            Throw();
+    }
+
+    //FixedUpdate is called at a fixed interval and is independent of frame rate. Put physics code here.
+    void FixedUpdate()
+    {
+        Vector2 movement = getMovementInput();
+        playerBody.AddForce (movement * speed);
+    }
+
+    public Vector2 getMovementInput()
+    {
+        //Store the current horizontal input in the float moveHorizontal.
+        float moveHorizontal = Input.GetAxis ("Horizontal");
+
+        //Store the current vertical input in the float moveVertical.
+        float moveVertical = Input.GetAxis ("Vertical");
+
+        //Use the two store floats to create a new Vector2 variable movement.
+        return new Vector2 (moveHorizontal, moveVertical);
+    }
+
+    void OnCollisionEnter2D(Collision2D col)
+    {
+        // If the player collided with the disc and does not already have possession
+        if (col.gameObject.tag == "disc" && !possession) {
+            // Create a joint to connect the disc to the player
+            RelativeJoint2D joint = playerBody.gameObject.AddComponent<RelativeJoint2D>();
+
+            // conects the joint to the other object
+            joint.connectedBody = disc;
+
+            // Stops objects from continuing to collide and creating more joints
+            joint.enableCollision = false;
+
+            // Slightly distance the disc from the player sprite
+            joint.linearOffset = new Vector2 (0, -0.3f);
+
+            // Remove mass from the disc to avoid slowing down player
+            disc.mass = 0;
+
+            // Set possession flag to True
+            possession = true;
+        }
+        // If the player collided with an ability pickup
+        else if (col.gameObject.tag == "dash") {
+            // Play the ability powerup SFX
+            soundEffects.Powerup();
+
+            // Destroy the pickup
+            Destroy(col.gameObject);
+
+            // Increment ability charges
+            abilityCount++;
+        }
+        // If the player collided with a spike
+        else if (col.gameObject.tag == "spike") {
+            soundEffects.Pop();
+            //arena.Respawn(gameObject);
+        }
+        // If the player collided with a wall
+        else if (col.gameObject.tag == "wall") {
+            soundEffects.Bounce();
+        }
+    }
+
+    void Throw()
+    {
+        // Destory the joint connecting the disc to the player
+        Destroy(GetComponent<RelativeJoint2D> () );
+
+        // Ignore collisions with the disc briefly while it moves away from the player
+        Physics2D.IgnoreCollision(disc.gameObject.GetComponent<Collider2D>(), GetComponent<Collider2D>(), true);
+
+        // Return the disc's mass to normal
+        disc.mass = 5;
+        
+        // Retrieve mouse in world position
+        Vector3 mousePos = Input.mousePosition;
+        mousePos.z = Camera.main.nearClipPlane;
+        Vector2 cursorInWorldPos = Camera.main.ScreenToWorldPoint( mousePos );
+        
+        // Calculate throw vector from the player towards the mouse
+        Vector2 mouseDelta =  cursorInWorldPos - playerBody.position;
+        mouseDelta.Normalize();
+
+        // Play the SFX for shooting
+        soundEffects.Shoot();
+
+        // Update the disc's velocity with the new vector scaled by this player's throw power
+        disc.velocity = playerBody.velocity + mouseDelta * gains;
+        
+        // Reset the possession flag to false
+        possession = false;
+
+        // Start a wait to return collision detection to the disc
+        StartCoroutine(DelayCollision());
+    }
+
+    IEnumerator DelayCollision() {
+        //Declare a yield instruction.
+        WaitForSeconds wait = new WaitForSeconds(1);
+
+        yield return wait; //Pause the loop for 1 second.
+        if (disc)
+            Physics2D.IgnoreCollision(disc.gameObject.GetComponent<Collider2D>(), GetComponent<Collider2D>(), false);
+   }
+}
--- Assets/Scripts/Scout.cs
+++ Assets/Scripts/Scout.cs
@@ -4,160 +4,57 @@ using UnityEngine.UI;
 using UnityEngine.Networking;
 using MLAPI;
 
-public class Scout : MonoBehaviour
+public class Scout : Player
 {
+    // Int representing dash power
+    public int legs;                    
 
-    public Camera playerCamera;
-    public float speed;                //Floating point variable to store the player's movement speed.
-    public int gains;
-    public int dashCount;
-    public int legs;
+    // UI text displayed for Scouts
     public string uiText = "Dash Remaining: ";
-    public bool isLocalPlayer = true;
 
-    private Arena arena;
-    private SoundEffects soundEffects;
-    private Rigidbody2D scout;        //Store a reference to the Rigidbody2D component required to use 2D Physics.
-    private SpriteRenderer player;
-    private Rigidbody2D ball;
-    private Rigidbody2D disc;
-    private Text dashUI;
+    // UI element which holds the uiText
+    private Text dashUI;                        
 
-    public bool trainingMode = false;
+    // Overrides Player Start
+    new void Start() {
+        // Call Player Start
+        base.Start();
 
-    private bool possession = false;
-
-    // Use this for initialization
-    void Start()
-    {
-        
-        if (isLocalPlayer)
-        {
-            playerCamera.enabled = true;
-        }
-        else
-        {
-            playerCamera.enabled = false;
-        }
-        
-        arena = FindObjectOfType<Arena>();
-        scout = GetComponent<Rigidbody2D>();
-        player = GetComponent<SpriteRenderer>();
-        soundEffects = GetComponent<SoundEffects>();
-        disc = GameObject.Find("Disc").GetComponent<Rigidbody2D>();
-        
+        // Retrieve DashUI Text Component from the Scene
         dashUI = GameObject.Find("dashUI").GetComponent<Text>();
-        scout.freezeRotation = true;
 
-        // Possibly change to a label or other identification later
-        trainingMode = arena.name == "TrainingArena";
-        // If Training is Enabled, enable the training mods
-        if (true == trainingMode)
+        // Initialize DashCount in the UI
+        dashUI.text = uiText + base.abilityCount.ToString();
+    }
+
+    // Overrides Player Update
+    new void Update()
+    {
+        // Call Player Update
+        base.Update();
+
+        // Check for scout specific Dash Input
+        if (abilityCount > 0 && Input.GetButtonDown("Jump"))
         {
-            TrainingMods training = gameObject.AddComponent<TrainingMods>() as TrainingMods;
-            // Give the Training Mods a reference to the disc
-            training.Load(disc);
-        }
-    }
-
-    void Update()
-    {
-        if (possession && Input.GetButtonDown("Fire1"))
-            Throw();
-
-        if (dashCount > 0 && Input.GetButtonDown("Jump"))
+            // Call Dash Function
             Dash();
-
-        dashUI.text = uiText + dashCount.ToString();
-    }
-
-    //FixedUpdate is called at a fixed interval and is independent of frame rate. Put physics code here.
-    void FixedUpdate()
-    {
-        Vector2 movement = getMovementInput();
-        scout.AddForce (movement * speed);
-    }
-
-    Vector2 getMovementInput()
-    {
-        //Store the current horizontal input in the float moveHorizontal.
-        float moveHorizontal = Input.GetAxis ("Horizontal");
-
-        //Store the current vertical input in the float moveVertical.
-        float moveVertical = Input.GetAxis ("Vertical");
-
-        //Use the two store floats to create a new Vector2 variable movement.
-        return new Vector2 (moveHorizontal, moveVertical);
-    }
-
-    void OnCollisionEnter2D(Collision2D col)
-    {
-        if (col.gameObject.tag == "disc" && !possession) {
-            RelativeJoint2D joint = scout.gameObject.AddComponent<RelativeJoint2D>();
-            ball = col.rigidbody;
-            // conects the joint to the other object
-            joint.connectedBody = ball;
-            // Stops objects from continuing to collide and creating more joints
-            joint.enableCollision = false;
-            joint.linearOffset = new Vector2 (0, -0.3f);
-
-            ball.mass = 0;
-            possession = true;
+            // Update dash count on the UI
+            dashUI.text = uiText + abilityCount.ToString();
         }
-        else if (col.gameObject.tag == "dash") {
-            soundEffects.Powerup();
-            Destroy(col.gameObject);
-            dashCount++;
-        }
-        else if (col.gameObject.tag == "spike") {
-            soundEffects.Pop();
-            //arena.Respawn(gameObject);
-        }
-        else if (col.gameObject.tag == "wall") {
-            soundEffects.Bounce();
-        }
-        
-
     }
-
-    IEnumerator DelayCollision() {
-        //Declare a yield instruction.
-        WaitForSeconds wait = new WaitForSeconds(1);
-
-        yield return wait; //Pause the loop for 1 second.
-        if (ball)
-            Physics2D.IgnoreCollision(ball.gameObject.GetComponent<Collider2D>(), GetComponent<Collider2D>(), false);
-   }
 
    void Dash()
    {
+        // Retrieve Key Inputs from player
         Vector2 movement = getMovementInput();
+
+        // Play the Dash Sound Effect
         soundEffects.Dash();
-        scout.velocity = movement * legs;
-        dashCount--;
+
+        // Update Player velocity
+        playerBody.velocity = movement * legs;
+        
+        // Decrement dash counter
+        abilityCount--;
    }
-
-    void Throw()
-    {
-        Destroy(GetComponent<RelativeJoint2D> () );
-        Physics2D.IgnoreCollision(ball.gameObject.GetComponent<Collider2D>(), GetComponent<Collider2D>(), true);
-        ball.mass = 5;
-        
-        Vector3 mousePos = Input.mousePosition;
-        mousePos.z = Camera.main.nearClipPlane;
-        Vector2 cursorInWorldPos = Camera.main.ScreenToWorldPoint( mousePos );
-        
-        Vector2 mouseDelta =  cursorInWorldPos - scout.position;
-        mouseDelta.Normalize();
-
-        soundEffects.Shoot();
-
-        ball.velocity = scout.velocity + mouseDelta * gains;
-        
-        possession = false;
-        float start = Time.time;
-        float curr = start;
-        StartCoroutine(DelayCollision());
-    }
-
 }
